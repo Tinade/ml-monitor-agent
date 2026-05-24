ce6a155ea8321a0eea29c00033fbe76a0a612252
Tsige Dessalgn <tsiged87@gmail.com>
2026-05-24 01:41:01 -0400

    Feature - Enabled Phonex

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
-cd ~/ml-monitor-agent
+cd ~/wsl-ml-monitor-agent
 source venv/bin/activate
 export GOOGLE_APPLICATION_CREDENTIALS=/home/tsige/.config/gcloud/application_default_credentials.json
 export GOOGLE_CLOUD_PROJECT=project-8a87dc48-0f93-4056-a1c
 export GOOGLE_CLOUD_LOCATION=us-central1
 export GOOGLE_GENAI_USE_VERTEXAI=1
 export PHOENIX_COLLECTOR_ENDPOINT='https://app.phoenix.arize.com/s/tsiged87'
-export PHOENIX_API_KEY='your-phoenix-key-here'
+export PHOENIX_API_KEY='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJBcGlLZXk6MSJ9.cdSoV_FTqCWrSIXfQJvB8WSYRtiMQAZ3t0VZncHA4XQ'

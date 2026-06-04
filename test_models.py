5ac2460ec20792cf848a7caf7e2de4e1fefdfabe
Tsige Dessalgn <tsiged87@gmail.com>
2026-06-03 23:47:57 -0400

    Adding test cases3

--- test_models.py
+++ test_models.py
@@ -7,3 +7,7 @@ def test_is_healthy_when_running():
 def test_is_healthy_when_stalled():
     job = Job("job_002", "stalled", 12, 0.99)
     assert job.is_healthy() == False
+
+def test_is_healthy_when_stalled():
+    job = Job("job_002", "failed", 0, None)
+    assert job.is_healthy() == False
